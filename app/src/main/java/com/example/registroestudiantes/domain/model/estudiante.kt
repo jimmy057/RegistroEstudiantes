3a615b0e5c4decf4e20bd01e2724091e37709d50
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-17 16:25:51 -0400

    agregando modelo de estudiante

---
+++ app/src/main/java/com/example/registroestudiantes/domain/model/estudiante.kt
@@ -0,0 +1,10 @@
+package com.example.registroestudiantes.domain.model
+
+data class Estudiante(
+    val estudianteId: Int = 0,
+    val nombres: String,
+    val email: String,
+    val edad: Int
+)
+
+
