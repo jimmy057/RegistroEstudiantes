b0c4edba927d7d1638bf37eaacd0eb3c859375f9
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-17 16:28:35 -0400

    agregando el nav host al main

--- app/src/main/java/com/example/registroestudiantes/MainActivity.kt
+++ app/src/main/java/com/example/registroestudiantes/MainActivity.kt
@@ -3,45 +3,25 @@ package com.example.registroestudiantes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.registroestudiantes.presentation.navigation.AppNavHost.AppNavHost
+import dagger.hilt.android.AndroidEntryPoint
 import com.example.registroestudiantes.ui.theme.RegistroEstudiantesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             RegistroEstudiantesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavHost(
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroEstudiantesTheme {
-        Greeting("Android")
-    }
-}
