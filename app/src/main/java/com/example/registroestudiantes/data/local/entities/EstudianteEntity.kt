5399b8c45184c59dfb5343f580e612dadd92e35a
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-17 16:23:35 -0400

    agregando y modificando la entidad de estudiantes con sus carpetas

---
+++ app/src/main/java/com/example/registroestudiantes/data/local/entities/EstudianteEntity.kt
@@ -0,0 +1,14 @@
+package com.example.registroestudiantes.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "estudiantes")
+data class EstudianteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val estudianteId: Int = 0,
+    val nombres: String,
+    val email: String,
+    val edad: Int
+)
+
