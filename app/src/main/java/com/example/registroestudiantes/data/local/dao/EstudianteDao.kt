23c04a60086ec4c9ae0426a2d6ab7ea09c2bfb0e
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-24 12:34:52 -0400

    modificando el dao la parte de existe nombre

--- app/src/main/java/com/example/registroestudiantes/data/local/dao/EstudianteDao.kt
+++ app/src/main/java/com/example/registroestudiantes/data/local/dao/EstudianteDao.kt
@@ -5,21 +5,38 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.registroestudiantes.data.local.entities.EstudianteEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EstudianteDao {
 
+    @Query("SELECT * FROM estudiantes ORDER BY nombres")
+    fun obtenerTodos(): Flow<List<EstudianteEntity>>
+
+    @Query("SELECT * FROM estudiantes WHERE estudianteId = :id")
+    suspend fun obtenerPorId(id: Int): EstudianteEntity?
+
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun insertar(estudiante: EstudianteEntity)
 
-    @Query("SELECT * FROM estudiantes")
-    suspend fun obtenerTodos(): List<EstudianteEntity>
-
-    @Query("SELECT COUNT(*) FROM estudiantes WHERE nombres = :nombre")
-    suspend fun existeNombre(nombre: String): Int
+    @Update
+    suspend fun actualizar(estudiante: EstudianteEntity)
 
     @Delete
     suspend fun eliminar(estudiante: EstudianteEntity)
+
+    @Query("""
+    SELECT COUNT(*) > 0 
+    FROM estudiantes 
+    WHERE nombres = :nombre 
+    AND estudianteId != :estudianteId
+""")
+    suspend fun existeNombre(
+        nombre: String,
+        estudianteId: Int
+    ): Boolean
+
 }
 
