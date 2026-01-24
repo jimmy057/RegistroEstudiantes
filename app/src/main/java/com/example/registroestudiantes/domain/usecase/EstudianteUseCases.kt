92bcc6e724ed2542047079f48cee35c176e57618
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-24 16:48:03 -0400

    modificando el use case de estudiante

--- app/src/main/java/com/example/registroestudiantes/domain/usecase/EstudianteUseCases.kt
+++ app/src/main/java/com/example/registroestudiantes/domain/usecase/EstudianteUseCases.kt
@@ -1,9 +1,31 @@
 package com.example.registroestudiantes.domain.usecase
 
-data class EstudianteUseCases(
-    val obtenerEstudiantes: ObtenerEstudiantesUseCase,
-    val obtenerPorId: ObtenerEstudiantePorIdUseCase,
-    val guardar: GuardarEstudianteUseCase,
-    val eliminar: EliminarEstudianteUseCase,
-    val existeNombre: ExisteNombreEstudianteUseCase
-)
+import com.example.registroestudiantes.domain.model.Estudiante
+import com.example.registroestudiantes.domain.repository.EstudianteRepository
+import kotlinx.coroutines.flow.Flow
+
+class EstudianteUseCases(
+    private val repository: EstudianteRepository
+) {
+    fun obtenerTodos(): Flow<List<Estudiante>> {
+        return repository.obtenerTodos()
+    }
+
+    suspend fun eliminar(estudiante: Estudiante) {
+        repository.eliminar(estudiante)
+    }
+
+    suspend fun guardar(estudiante: Estudiante) {
+        repository.guardar(estudiante)
+    }
+
+    suspend fun obtenerPorId(id: Int): Estudiante? {
+        return repository.obtenerPorId(id)
+    }
+
+    suspend fun existeNombre(nombres: String, estudianteId: Int = 0): Boolean {
+        return repository.existeNombre(nombres, estudianteId)
+    }
+
+}
+
