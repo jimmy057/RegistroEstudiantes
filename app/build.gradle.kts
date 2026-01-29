354c461c0b7a262d73bfc4c0d4ad53c2e28a21ef
jimmy057 <jimmypolanco2540@gmail.com>
2026-01-28 20:16:50 -0400

    renombrando el hilt app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,6 @@ dependencies {
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
-
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
