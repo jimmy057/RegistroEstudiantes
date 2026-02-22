0700e7f9d3e3fd297955b763f18aafd6359932ab
jimmy057 <jimmypolanco2540@gmail.com>
2026-02-22 16:13:56 -0400

    agregando tipopenalidad a la base de datos

--- app/src/main/java/com/example/registroestudiantes/data/local/databases/AppDatabase.kt
+++ app/src/main/java/com/example/registroestudiantes/data/local/databases/AppDatabase.kt
@@ -4,15 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.registroestudiantes.data.local.dao.AsignaturaDao
 import com.example.registroestudiantes.data.local.dao.EstudianteDao
+import com.example.registroestudiantes.data.local.dao.TipoPenalidadDao
 import com.example.registroestudiantes.data.local.entities.AsignaturaEntity
 import com.example.registroestudiantes.data.local.entities.EstudianteEntity
+import com.example.registroestudiantes.data.local.entities.TipoPenalidadEntity
 
 @Database(
     entities = [
         EstudianteEntity::class,
-        AsignaturaEntity::class
+        AsignaturaEntity::class,
+        TipoPenalidadEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -20,4 +23,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun estudianteDao(): EstudianteDao
 
     abstract fun asignaturaDao(): AsignaturaDao
+
+    abstract fun tipoPenalidadDao(): TipoPenalidadDao
 }
