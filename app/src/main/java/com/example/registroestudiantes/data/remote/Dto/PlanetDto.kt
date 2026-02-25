2cb368801c40da9eda3a16ea61319739b6b89223
jimmy057 <jimmypolanco2540@gmail.com>
2026-02-24 21:28:48 -0400

    agregando la carpeta de remote y agregando el Dto y el dtoresponse

---
+++ app/src/main/java/com/example/registroestudiantes/data/remote/Dto/PlanetDto.kt
@@ -0,0 +1,8 @@
+package com.example.registroestudiantes.data.remote.Dto
+
+data class PlanetDto(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val image: String
+)
---
+++ app/src/main/java/com/example/registroestudiantes/data/remote/Dto/PlanetResponseDto.kt
@@ -0,0 +1,5 @@
+package com.example.registroestudiantes.data.remote.Dto
+
+data class PlanetResponseDto(
+    val items: List<PlanetDto>
+)
