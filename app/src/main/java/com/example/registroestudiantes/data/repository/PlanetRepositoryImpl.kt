4a53c7b88b2def13cc40f20a77a1ff96edf49cca
jimmy057 <jimmypolanco2540@gmail.com>
2026-03-09 19:59:41 -0400

    modificandi el repositoryimpl para que acepte la carpeta de resource

--- app/src/main/java/com/example/registroestudiantes/data/repository/PlanetRepositoryImpl.kt
+++ app/src/main/java/com/example/registroestudiantes/data/repository/PlanetRepositoryImpl.kt
@@ -1,19 +1,55 @@
 package com.example.registroestudiantes.data.repository
 
+import com.example.registroestudiantes.data.Resource.Resource
 import com.example.registroestudiantes.data.local.mapper.toDomain
-import com.example.registroestudiantes.data.remote.PlanetApiService
+import com.example.registroestudiantes.data.remote.PlanetApi
 import com.example.registroestudiantes.domain.model.Planet
 import com.example.registroestudiantes.domain.repository.PlanetRepository
+import javax.inject.Inject
 
 class PlanetRepositoryImpl(
-    private val api: PlanetApiService
+    private val api: PlanetApi
 ) : PlanetRepository {
 
-    override suspend fun getPlanets(): List<Planet> {
-        return api.getPlanets().items.map { it.toDomain() }
+    override suspend fun getPlanets(): Resource<List<Planet>> {
+
+        return try {
+
+            val response = api.getPlanets()
+
+            if (response.isSuccessful && response.body() != null) {
+
+                val planets = response.body()!!.items.map {
+                    it.toDomain()
+                }
+
+                Resource.Success(planets)
+
+            } else {
+                Resource.Error("Error servidor")
+            }
+
+        } catch (e: Exception) {
+            Resource.Error("Error conexión")
+        }
     }
 
-    override suspend fun getPlanetById(id: Int): Planet {
-        return api.getPlanetById(id).toDomain()
+    override suspend fun getPlanetById(id: Int): Resource<Planet> {
+
+        return try {
+
+            val response = api.getPlanetById(id)
+
+            if (response.isSuccessful && response.body() != null) {
+
+                Resource.Success(response.body()!!.toDomain())
+
+            } else {
+                Resource.Error("No encontrado")
+            }
+
+        } catch (e: Exception) {
+            Resource.Error("Error conexión")
+        }
     }
 }
