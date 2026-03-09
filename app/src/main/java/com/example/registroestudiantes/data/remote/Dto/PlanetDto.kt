3c42570049946dcb90b2e386130318e36f4bd371
jimmy057 <jimmypolanco2540@gmail.com>
2026-03-09 19:56:58 -0400

    modificando el dto a la version correcta

--- app/src/main/java/com/example/registroestudiantes/data/remote/Dto/PlanetDto.kt
+++ app/src/main/java/com/example/registroestudiantes/data/remote/Dto/PlanetDto.kt
@@ -1,8 +1,19 @@
 package com.example.registroestudiantes.data.remote.Dto
 
+import com.example.registroestudiantes.domain.model.Planet
+
 data class PlanetDto(
     val id: Int,
     val name: String,
     val description: String,
     val image: String
-)
+) {
+    fun toDomain(): Planet {
+        return Planet(
+            id = id,
+            name = name,
+            description = description,
+            image = image
+        )
+    }
+}
