546cdf484ea6f485baeaed795a8359dcffa5d163
jimmy057 <jimmypolanco2540@gmail.com>
2026-03-09 19:59:26 -0400

    modificandi el domain repository para que acepte la carpeta de resource

--- app/src/main/java/com/example/registroestudiantes/domain/repository/PlanetRepository.kt
+++ app/src/main/java/com/example/registroestudiantes/domain/repository/PlanetRepository.kt
@@ -1,10 +1,9 @@
 package com.example.registroestudiantes.domain.repository
 
+import com.example.registroestudiantes.data.Resource.Resource
 import com.example.registroestudiantes.domain.model.Planet
 
 interface PlanetRepository {
-
-    suspend fun getPlanets(): List<Planet>
-
-    suspend fun getPlanetById(id: Int): Planet
+    suspend fun getPlanets(): Resource<List<Planet>>
+    suspend fun getPlanetById(id: Int): Resource<Planet>
 }
