37b45d1f8677cc34d48b85ffd78337a1d1386a11
jimmy057 <jimmypolanco2540@gmail.com>
2026-03-09 19:58:36 -0400

    modificando los use case

--- app/src/main/java/com/example/registroestudiantes/domain/usecase/PlanetUseCase/GetPlanetByIdUseCase.kt
+++ app/src/main/java/com/example/registroestudiantes/domain/usecase/PlanetUseCase/GetPlanetByIdUseCase.kt
@@ -1,9 +1,13 @@
 package com.example.registroestudiantes.domain.usecase.PlanetUseCase
 
+import com.example.registroestudiantes.data.Resource.Resource
+import com.example.registroestudiantes.domain.model.Planet
 import com.example.registroestudiantes.domain.repository.PlanetRepository
 
 class GetPlanetByIdUseCase(
     private val repository: PlanetRepository
 ) {
-    suspend operator fun invoke(id: Int) = repository.getPlanetById(id)
+    suspend operator fun invoke(id: Int): Resource<Planet> {
+        return repository.getPlanetById(id)
+    }
 }
--- app/src/main/java/com/example/registroestudiantes/domain/usecase/PlanetUseCase/GetPlanetsUseCase.kt
+++ app/src/main/java/com/example/registroestudiantes/domain/usecase/PlanetUseCase/GetPlanetsUseCase.kt
@@ -1,9 +1,13 @@
 package com.example.registroestudiantes.domain.usecase.PlanetUseCase
 
+import com.example.registroestudiantes.data.Resource.Resource
+import com.example.registroestudiantes.domain.model.Planet
 import com.example.registroestudiantes.domain.repository.PlanetRepository
 
 class GetPlanetsUseCase(
     private val repository: PlanetRepository
 ) {
-    suspend operator fun invoke() = repository.getPlanets()
+    suspend operator fun invoke(): Resource<List<Planet>> {
+        return repository.getPlanets()
+    }
 }
